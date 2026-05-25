31678e86fce95cc3dcd06245ecdb65add6641523
Nathanael Lambert <nathanael.lambert@protonmail.com>
2026-05-25 14:29:50 +0200

    cache

--- matching.py
+++ matching.py
@@ -14,9 +14,9 @@ RANSAC_THRESHOLD = 3.0
 RANSAC_CONFIDENCE = 0.99
 RANSAC_MAX_ITERATIONS = 500
   
-def _best_matching_pair_with_confidence(img_url: str, longitude: float, latitude: float)-> Tuple[Optional[MapillaryPicture], float]:
+def _best_matching_pair_with_confidence(cache, img_url: str, longitude: float, latitude: float)-> Tuple[Optional[MapillaryPicture], float]:
 
-    flickr_pic = load_picture(img_url)
+    flickr_pic = cache.get(img_url)
     if not flickr_pic:
         return None, 0
     sampler = create_sampler(longitude=longitude, latitude=latitude, st_km=SAMPLING_RADIUS)
@@ -27,7 +27,7 @@ def _best_matching_pair_with_confidence(img_url: str, longitude: float, latitude
         candidate = sampler.sample_candidates()
         if not candidate:
             return None, 0
-        mapillary_pic = load_picture(candidate.pic_url)
+        mapillary_pic = cache.get(candidate.pic_url)
         if not mapillary_pic:
             continue
         kp0, kp1 = compute_loftr_matches(flickr_pic, mapillary_pic)
@@ -42,10 +42,10 @@ def _best_matching_pair_with_confidence(img_url: str, longitude: float, latitude
 
 
 
-def find_matches(df: pd.DataFrame)-> pd.DataFrame:
+def find_matches(df: pd.DataFrame, cache)-> pd.DataFrame:
 
     for row in df:
-        mapillary_pic, confidence = _best_matching_pair_with_confidence(row['url_o'], row['longitude'], row['latitude'])
+        mapillary_pic, confidence = _best_matching_pair_with_confidence(cache, row['url_o'], row['longitude'], row['latitude'])
     
     df[["mappilary_id", "mapillary_lon", "mapillary_lat" ,"p_match", "mapillary_compass_angle",
      "mapillary_captured_at", "mapillary_pic_url"]] = 
